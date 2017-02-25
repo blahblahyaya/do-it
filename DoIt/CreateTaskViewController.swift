aa2b9e24f9d0391dd9125513c403bc41db1c96d3
Andy Roberts <andy.roberts@avalara.com>
2017-02-25 13:40:24 -0800

    main and add before save logic

---
+++ DoIt/CreateTaskViewController.swift
@@ -0,0 +1,23 @@
+//
+//  CreateTaskViewController.swift
+//  DoIt
+//
+//  Created by Andy Roberts (Contractor) on 2/25/17.
+//  Copyright © 2017 Andy Roberts. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var taskNameTextField: UITextField!
+    @IBOutlet weak var importantSwitch: UISwitch!
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ DoIt/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Andy Roberts (Contractor) on 2/25/17.
+//  Copyright © 2017 Andy Roberts. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
